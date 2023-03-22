ac204c82df94a2027b2ffe157f95fb072dc0ed01
galargh <piotr.galar@gmail.com>
2023-03-22 15:12:37 +0100

    Revert "Revert allow more resources to be managed"
    This reverts commit dd46fe8a9d9576c77e9f3aa7d45c6427ddb7ce86.

--- terraform/locals_override.tf
+++ terraform/locals_override.tf
@@ -2,7 +2,13 @@
 # By default, none of the resource types are managed through github-mgmt.
 locals {
   resource_types = [
+    "github_membership",
+    "github_repository_collaborator",
     "github_repository",
-    "github_branch_protection"
+    "github_team_membership",
+    "github_team_repository",
+    "github_team",
+    "github_branch_protection",
+    "github_repository_file"
   ]
 }
