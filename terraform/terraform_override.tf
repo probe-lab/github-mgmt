22cef581172504085d32574430699e4ed9cfcfa5
Ian Davis <nospam@iandavis.com>
2023-03-22 11:52:15 +0000

    Initial confiiguration

--- terraform/terraform_override.tf
+++ terraform/terraform_override.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     region               = "us-east-1"
-    bucket               = "github-mgmt"
+    bucket               = "probelab-infra-github-mgmt"
     key                  = "terraform.tfstate"
     workspace_key_prefix = "org"
     dynamodb_table       = "github-mgmt"
