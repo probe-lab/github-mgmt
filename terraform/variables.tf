66766b0ac815d7f2668ddd8e875908facf147367
galargh <galargh@users.noreply.github.com>
2025-05-17 15:32:06 +0000

    upgrade@15086630597

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,18 @@ variable "write_delay_ms" {
   type        = number
   default     = 1000
 }
+
+variable "resources" {
+  description = "Resources to import."
+  type = object({
+    github_membership              = map(object)
+    github_repository              = map(object)
+    github_repository_collaborator = map(object)
+    github_branch_protection       = map(object)
+    github_team                    = map(object)
+    github_team_repository         = map(object)
+    github_team_membership         = map(object)
+    github_repository_file         = map(object)
+    github_issue_labels            = map(object)
+  })
+}
